76d0eb89c450f1ca560b097226ab59155172b31c
James Jackson <james.sorley.jackson@gmail.com>
2020-12-22 13:53:14 -0600

    ENG-15: Decouple specific keystrokes from logic handling the keybuffer transformation (in KeyBuffer)
    https://linear.app/fig/issue/ENG-15/decouple-specific-keystrokes-from-logic-handling-the-keybuffer
    Signed-off-by: James Jackson <james.sorley.jackson@gmail.com>

---
+++ fig/Autocompletion/Keystroke.swift
@@ -0,0 +1,165 @@
+//
+//  Keystroke.swift
+//  fig
+//
+//  Created by James Jackson on 12/22/20.
+//  Copyright © 2020 Matt Schrage. All rights reserved.
+//
+
+import Cocoa
+
+struct Keystroke: Hashable {
+  var modifierFlags: NSEvent.ModifierFlags = []
+  var keyCode: UInt16
+  
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(keyCode)
+    hasher.combine(modifierFlags)
+  }
+  
+  static func == (lhs: Keystroke, rhs: Keystroke) -> Bool {
+    var isEqual = lhs.keyCode == rhs.keyCode
+    // ignore modifiers if none specified
+    if (!(lhs.modifierFlags.isEmpty || rhs.modifierFlags.isEmpty)) {
+      isEqual = isEqual && lhs.modifierFlags == rhs.modifierFlags
+    }
+    return isEqual
+  }
+}
+
+extension NSEvent.ModifierFlags: Hashable {
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(self.carbonFlags)
+  }
+}
+
+var keyBindings: [Keystroke: TextTransformation] = [
+  // paste
+  Keystroke(modifierFlags: [.command], keyCode: Keycode.v) : .paste,
+  // backward-word
+  Keystroke(keyCode: Keycode.leftArrow): .backwardWord,
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.b) : .backwardWord,
+  // forward-word
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.f) : .forwardWord,
+  Keystroke(keyCode: Keycode.rightArrow) : .forwardWord,
+  // history-search-backward
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.p) : .historySearchBackward,
+  Keystroke(keyCode: Keycode.upArrow) : .historySearchBackward,
+  // history-search-forward
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.n) : .historySearchForward,
+  Keystroke(keyCode: Keycode.downArrow) : .historySearchForward,
+  // beginning-of-line
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.a) : .beginningOfLine,
+  // end-of-line
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.e) : .endOfLine,
+  // history-incremental-search-backward
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.r) : .historyIncrementalSearchBackward,
+  // history-incremental-search-forward
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.s) : .historyIncrementalSearchForward,
+  // backward-delete-char
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.h) : .backwardDeleteChar,
+  Keystroke(keyCode: Keycode.delete) : .backwardDeleteChar,
+  // delete-char-or-list
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.d) : .deleteCharOrList,
+  // transpose-chars
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.t) : .transposeChars,
+  // kill-whole-line
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.u) : .killWholeLine,
+  // backward-kill-word
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.w) : .backwardKillWord,
+  // yank
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.y) : .yank,
+  // send-break
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.g) : .sendBreak,
+  // accept-line
+  Keystroke(keyCode: Keycode.returnKey) : .acceptLine,
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.j) : .acceptLine,
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.m) : .acceptLine,
+  // expand-or-complete
+  Keystroke(keyCode: Keycode.tab) : .expandOrComplete,
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.i) : .expandOrComplete,
+  
+  // temp
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.forwardSlash) : .forwardSlash,
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.c) : .killProcess,
+  Keystroke(modifierFlags: [.control], keyCode: Keycode.two) : .ctrlTwo,
+]
+
+enum TextTransformation {
+  case paste
+  case backwardWord
+  case forwardWord
+  case historySearchBackward
+  case historySearchForward
+  case setMarkCommand
+  case beginningOfLine
+  case backwardChar
+  case deleteCharOrList
+  case endOfLine
+  case forwardChar
+  case sendBreak
+  case backwardDeleteChar
+  case expandOrComplete
+  case acceptLine
+  case killLine
+  case clearScreen
+  case downLineOrHistory
+  case acceptLineAndDownHistory
+  case upLineOrHistory
+  case pushLine
+  case historyIncrementalSearchBackward
+  case historyIncrementalSearchForward
+  case transposeChars
+  case killWholeLine
+  case quotedInsert
+  case backwardKillWord
+  case viMatchBracket
+  case viFindNextchar
+  case viJoin
+  case killBuffer
+  case inferNextHistory
+  case overwriteMode
+  case undo
+  case viCmdMode
+  case exchangePointAndMark
+  case expandWord
+  case whatCursorPosition
+  case listExpand
+  case yank
+  case listChoices
+  case selfInsertUnmeta
+  case copyPrevWord
+  case expandHistory
+  case quoteRegion
+  case spellWord
+  case quoteLine
+  case negArgument
+  case insertLastWord
+  case digitArgument
+  case beginningOfBufferOrHistory
+  case endFfBufferOrHistory
+  case whichCommand
+  case acceptAndHold
+  case capitalizeWord
+  case killWord
+  case getLine
+  case runHelp
+  case downCaseWord
+  case upLineOrSearch
+  case downLineOrSearch
+  case transposeWords
+  case upCaseWord
+  case copyRegionAsKill
+  case bracketedPaste
+  case deleteChar
+  case executeNamedCmd
+  case yankPop
+  case executeLastNamedCmd
+  case viGotoColumn
+  case selfInsert
+  // temp
+  case forwardSlash
+  case ctrlTwo
+  case killProcess
+}
+
---
+++ fig/Logger.swift
@@ -0,0 +1,77 @@
+//
+//  Logger.swift
+//  fig
+//
+//  Created by Matt Schrage on 5/27/20.
+//  Copyright © 2020 Matt Schrage. All rights reserved.
+//
+
+import os.log
+import Foundation
+
+class Logger {
+  
+  enum Priority {
+    case info
+    case notify
+  }
+  
+  enum Subsystem: String {
+    case global = "global"
+    case telemetry = "telemetry"
+    case windowServer = "windowserver"
+    case keypress = "keypress"
+    case xterm = "xterm"
+    case javascript = "javascript"
+  }
+  
+  static var defaultLocation: URL = URL(fileURLWithPath: "\(NSHomeDirectory())/.fig/debug.log")
+  static func log(message: String, priority: Priority = .info, subsystem: Subsystem = .global) {
+    print(message)
+    let line = Logger.format(message, priority, subsystem)
+    appendToLog(line)
+    
+    //|| Defaults.broadcastLogsForSubsystem == subsystem
+    if Defaults.broadcastLogs {
+      let notification = NSUserNotification()
+      notification.title = "Logging: \(subsystem.rawValue)"
+      notification.subtitle = message
+      NSUserNotificationCenter.default.deliver(notification)
+    }
+  }
+  
+  fileprivate static func appendToLog(_ line: String) {
+    if let file = try? FileHandle(forUpdating: Logger.defaultLocation) {
+      file.seekToEndOfFile()
+      file.write(line.data(using: .utf8)!)
+      file.closeFile()
+    } else {
+      do {
+        try line.write(to: defaultLocation, atomically: true, encoding: String.Encoding.utf8)
+      } catch {
+        print("debug.log does not exist and could not be created. Logs will not be written.")
+      }
+    }
+  }
+  
+  static func format(_ message: String,_ priority: Priority,_ subsystem: Subsystem) ->String {
+    let prefix = "\(subsystem.rawValue) | \(Logger.now): "
+    return message.split(separator: "\n").map { return prefix + $0 }.joined(separator: "\n") + "\n"
+  }
+  
+  static var now: String {
+    let now = Date()
+    let formatter = DateFormatter()
+    formatter.timeZone = TimeZone.current
+    formatter.dateFormat = "yyyy-MM-dd HH:mm"
+    return formatter.string(from: now)
+  }
+}
+
+extension OSLog {
+  private static var subsystem = Bundle.main.bundleIdentifier!
+  
+  /// Logs the view cycles like viewDidLoad.
+  static let socketServer = OSLog(subsystem: subsystem, category: "socketServer")
+  static let xterm = OSLog(subsystem: subsystem, category: "fig.xterm")
+}
