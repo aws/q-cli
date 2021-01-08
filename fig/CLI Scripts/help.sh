dd91455c8cb2ca3ee72c922981eb809db565094d
Matt Schrage <matthewschrage@gmail.com>
2021-01-07 16:12:48 -0800

    Add uninstall to help text

--- fig/CLI Scripts/help.sh
+++ fig/CLI Scripts/help.sh
@@ -15,11 +15,12 @@ ${BOLD}USAGE${NORMAL}
   $ fig [COMMAND]
 
 ${BOLD}COMMANDS${NORMAL}
-
   invite        invite up to 5 friends & teammates to Fig
   update        update repo of completion scripts
   docs          documentation for building completion specs
   source        (re)connect fig to the current shell session
   list          print all commands with completion specs
+  uninstall     remove a completion spec
   --help        a summary of the Fig CLI commands
+  
 EOF
