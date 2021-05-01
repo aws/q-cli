ef544ca399742981d67125724e823f5733316041
Matt Schrage <matthewschrage@gmail.com>
2021-04-30 18:43:53 -0700

    feat: update script to pull specs from waitlist.withfig.com/specs

--- fig/CLI Scripts/update.sh
+++ fig/CLI Scripts/update.sh
@@ -7,7 +7,7 @@ echo "Run $(tput setaf 5)$(tput bold)fig docs$(tput sgr0) to learn how to contri
 echo
 # Download all the files in the specs folder of this repo
 
-base_url='https://codeload.github.com/withfig/autocomplete/tar.gz/';
+base_url='https://waitlist.withfig.com/specs?version=';
 
 # This is the current version of autocomplete
 # It should be 1 ahead of the most recent branch we have created on github
@@ -16,6 +16,4 @@ current_version=$(defaults read com.mschrage.fig "autocompleteVersion" 2>/dev/nu
 
 mkdir -p ~/.fig/autocomplete; cd $_
 
-curl -s $base_url$current_version --fail --silent --head > /dev/null 2>/dev/null \
-&& curl -s $base_url$current_version | tar -xz  --strip-components=2 autocomplete-master/specs \
-|| curl -s $base_url'master' | tar -xz --strip-components=2 autocomplete-master/specs
+curl -s "$base_url$current_version" | tar -xz --strip-components=1 specs
