f57c97671723baf78a9ed9e15800190e838faee1
Matt Schrage <matt@fig.io>
2021-10-25 13:22:23 -0700

    Add support for FIG_NEW_SESSION environment variable

--- config/shell/pre.fish
+++ config/shell/pre.fish
@@ -1,6 +1,13 @@
 contains $HOME/.fig/bin $fish_user_paths
 or set -Ua fish_user_paths $HOME/.fig/bin
 
+if [ ! -z "$FIG_NEW_SESSION" ]
+  set --erase TERM_SESSION_ID
+  set --erase FIG_TERM
+  set --erase FIG_ENV_VAR
+  set --erase FIG_NEW_SESSION
+end
+
 if [ -d /Applications/Fig.app -o -d ~/Applications/Fig.app ] \
   && command -v fig 1>/dev/null 2>/dev/null \
   && [ -t 1 ] \
@@ -10,7 +17,7 @@ if [ -d /Applications/Fig.app -o -d ~/Applications/Fig.app ] \
   # Generated automatically by iTerm and Terminal But needs to be
   # explicitly set for VSCode and Hyper. This variable is inherited when
   # new ttys are created using tmux and must be explictly overwritten.
-  if [ -z "$TERM_SESSION_ID" ] || [ -n "$TMUX" ] # || [ "$TERM_PROGRAM" = vscode ]
+  if [ -z "$TERM_SESSION_ID" ] || [ -n "$TMUX" ]
     export TERM_SESSION_ID=(uuidgen)
   end
   export FIG_INTEGRATION_VERSION=5
