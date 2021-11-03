c796ecf8090e24dfe99303dc8ff078500384ebf2
Matt Schrage <matt@fig.io>
2021-11-03 16:35:24 -0700

    Remove python script

--- fig/upgrade/1.0.23.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/.fig/autocomplete; cd $_
-
-# Download all the files in the specs folder of this repo
-curl https://codeload.github.com/withfig/completion-specs/tar.gz/master | \
-tar -xz --strip=2 completion-specs-master/specs
-
-TEMPALIAS='function cd() { builtin cd "$1"; fig bg:cd; }\nfig bg:cd'
-grep -q $TEMPALIAS ~/.fig/exports/env.sh || echo '\n\n'$TEMPALIAS'\n\n' >> ~/.fig/exports/env.sh
-
