beab0b5fe2f4daa1cae8fbc0103e4675aed6fb10
grant0417 <grant@fig.io>
2021-11-11 15:11:39 -0500

    Clean up of CLI functions

--- figcli2/cmd/launch/launch.go
+++ figcli2/cmd/launch/launch.go
@@ -1,6 +1,7 @@
 package launch
 
 import (
+	"fmt"
 	"os/exec"
 
 	"github.com/spf13/cobra"
@@ -12,6 +13,7 @@ func NewCmdLaunch() *cobra.Command {
 		Short:  "Launch Fig",
 		Hidden: true,
 		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("→ Launching Fig...")
 			figCmd := exec.Command("open", "-b", "com.mschrage.fig")
 			figCmd.Run()
 			figCmd.Process.Release()
