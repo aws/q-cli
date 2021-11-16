c6d5acbe91817c51e98560d7fd85cb03954abee4
grant0417 <grant@fig.io>
2021-11-16 08:36:51 -0500

    CLI Cleanup

--- figcli2/cmd/launch/launch.go
+++ figcli2/cmd/launch/launch.go
@@ -1,11 +1,30 @@
 package launch
 
 import (
+	"fig-cli/logging"
 	"fmt"
+	"os"
 	"os/exec"
 
 	"github.com/spf13/cobra"
 )
+
+func Launch() {
+	fmt.Printf("\n→ Launching Fig...\n\n")
+	figCmd := exec.Command("open", "-b", "com.mschrage.fig")
+
+	if err := figCmd.Run(); err != nil {
+		fmt.Printf("\n→ Fig could not be launched.\n\n")
+		logging.Log("restart:", err.Error())
+		os.Exit(1)
+	}
+
+	if err := figCmd.Process.Release(); err != nil {
+		fmt.Printf("\n→ Fig could not be launched.\n\n")
+		logging.Log("restart:", err.Error())
+		os.Exit(1)
+	}
+}
 
 func NewCmdLaunch() *cobra.Command {
 	cmd := &cobra.Command{
@@ -13,10 +32,7 @@ func NewCmdLaunch() *cobra.Command {
 		Short:  "Launch Fig",
 		Hidden: true,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("\n→ Launching Fig...\n\n")
-			figCmd := exec.Command("open", "-b", "com.mschrage.fig")
-			figCmd.Run()
-			figCmd.Process.Release()
+			Launch()
 		},
 	}
 
