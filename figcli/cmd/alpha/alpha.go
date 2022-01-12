eb6a9d890ee67d2b405b1b72ab80a89b1299ac58
grant0417 <grant@fig.io>
2022-01-11 23:18:05 -0500

    cli: Add dotfile downloading to alpha

--- figcli/cmd/alpha/alpha.go
+++ figcli/cmd/alpha/alpha.go
@@ -2,7 +2,8 @@ package alpha
 
 import (
 	"fig-cli/cmd/alpha/source"
-	"fig-cli/logging"
+	"fmt"
+	"os"
 	"os/exec"
 
 	"github.com/spf13/cobra"
@@ -15,8 +16,11 @@ func NewCmdAlpha() *cobra.Command {
 		Run: func(cmd *cobra.Command, args []string) {
 			// TODO: send protobuf to open local mission control
 			if err := exec.Command("open", "https://fig.io").Run(); err != nil {
-				logging.Log("Unable to open mission control", err.Error())
+				fmt.Printf("Error opening mission control: %s", err)
+				os.Exit(1)
 			}
+
+			fmt.Printf("\n→ Opening mission control\n\n")
 		},
 	}
 
