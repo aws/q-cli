3d42619e29c7c0644909ec25b6ea319807242c98
Matt Schrage <matthewschrage@gmail.com>
2022-01-12 17:40:22 -0800

    Load mission control correctly

--- fig/MissionControl.swift
+++ fig/MissionControl.swift
@@ -38,7 +38,7 @@ class MissionControl {
       }
 
       // otherwise use fallback
-      return Remote.baseURL.appendingPathComponent("mission-control")
+      return Remote.baseURL.appendingPathComponent("mission-control", isDirectory: true)
     }()
 
     let viewController = WebViewController()
