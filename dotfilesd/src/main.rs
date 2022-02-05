bcd6fddf94474f297fd24f6248c6b9c11b30f0a7
grant0417 <grantgurvis@gmail.com>
2022-02-04 19:42:26 -0800

    dotfilesd: Add sentry logging

--- dotfilesd/src/main.rs
+++ dotfilesd/src/main.rs
@@ -1,4 +1,5 @@
 use clap::StructOpt;
+use util::auth::get_email;
 
 pub mod auth;
 pub mod cli;
@@ -11,5 +12,20 @@ pub mod util;
 
 #[tokio::main]
 async fn main() {
+    let _guard = sentry::init((
+        "https://0631fceb9ae540bb874af81820507ebf@o436453.ingest.sentry.io/6187837",
+        sentry::ClientOptions {
+            release: sentry::release_name!(),
+            ..Default::default()
+        },
+    ));
+
+    sentry::configure_scope(|scope| {
+        scope.set_user(Some(sentry::User {
+            email: get_email(),
+            ..Default::default()
+        }));
+    });
+
     cli::Cli::parse().execute().await;
 }
