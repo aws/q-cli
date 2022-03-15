25d40c76afe3359e33003687ec14ecbbc1728eb8
grant0417 <grantgurvis@gmail.com>
2022-03-14 18:34:04 -0700

    figcli(daemon): hook up scheduler to to basic tasks

--- figcli/src/daemon/scheduler.rs
+++ figcli/src/daemon/scheduler.rs
@@ -1,15 +1,15 @@
-use std::{borrow::Cow, collections::BinaryHeap, pin::Pin, time::Duration};
+use std::{borrow::Cow, collections::BinaryHeap, time::Duration};
 
 use anyhow::Result;
 use async_trait::async_trait;
-use futures::{future, stream::FuturesUnordered, FutureExt, StreamExt};
+use rand::{distributions::Uniform, prelude::Distribution};
 use std::fmt::Debug;
-use tokio::time::{sleep_until, Instant, Sleep};
+use tokio::time::{sleep_until, Instant};
 
 use crate::{cli::source::sync_all_shells, plugins::api::fetch_installed_plugins};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-struct Tag<'a>(Cow<'a, str>);
+pub struct Tag<'a>(Cow<'a, str>);
 
 impl<'a, S> From<S> for Tag<'a>
 where
@@ -21,25 +21,21 @@ where
 }
 
 #[derive(Debug)]
-struct ScheduledTask {
-    sleep: Sleep,
+pub struct ScheduledTask {
+    time: Instant,
     tag: Tag<'static>,
     task: Box<dyn Task>,
 }
 
 impl ScheduledTask {
-    fn new(sleep: Sleep, tag: Tag<'static>, task: Box<dyn Task>) -> Self {
-        ScheduledTask {
-            sleep: sleep,
-            tag: tag,
-            task: task,
-        }
+    fn new(time: Instant, tag: Tag<'static>, task: Box<dyn Task>) -> Self {
+        ScheduledTask { time, tag, task }
     }
 }
 
 impl std::cmp::Ord for ScheduledTask {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.sleep.deadline().cmp(&other.sleep.deadline())
+        self.time.cmp(&other.time).reverse()
     }
 }
 
@@ -53,7 +49,7 @@ impl std::cmp::Eq for ScheduledTask {}
 
 impl std::cmp::PartialEq for ScheduledTask {
     fn eq(&self, other: &Self) -> bool {
-        self.sleep.deadline() == other.sleep.deadline()
+        self.time == other.time
     }
 }
 
@@ -61,26 +57,37 @@ pub struct Scheduler {
     scheduled_tasks: BinaryHeap<ScheduledTask>,
 }
 
+impl Default for Scheduler {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Scheduler {
-    fn new() -> Self {
+    pub fn new() -> Self {
+        // A task that is scheduled that will never be executed
+        // but will not overflow the time primitive
+        let final_task = ScheduledTask::new(
+            Instant::now() + Duration::from_secs(86400 * 365 * 30),
+            Tag::from("final"),
+            Box::new(NoopTask),
+        );
+
         Self {
-            scheduled_tasks: BinaryHeap::new(),
+            scheduled_tasks: [final_task].into_iter().collect(),
         }
     }
 
-    fn schedule<T, B>(&mut self, task: T, when: Instant)
+    pub fn schedule<T, B>(&mut self, task: T, when: Instant)
     where
         T: TaggedTask + Into<Box<B>>,
         B: Task + 'static,
     {
-        self.scheduled_tasks.push(ScheduledTask::new(
-            sleep_until(when),
-            task.tag(),
-            task.into(),
-        ));
+        self.scheduled_tasks
+            .push(ScheduledTask::new(when, task.tag(), task.into()));
     }
 
-    fn schedule_delayed<T, B>(&mut self, task: T, delay: Duration)
+    pub fn schedule_delayed<T, B>(&mut self, task: T, delay: Duration)
     where
         T: TaggedTask + Into<Box<B>>,
         B: Task + 'static,
@@ -88,16 +95,16 @@ impl Scheduler {
         self.schedule(task, Instant::now() + delay);
     }
 
-    fn schedule_with_tag<T, B>(&mut self, task: T, when: Instant, tag: Tag<'static>)
+    pub fn schedule_with_tag<T, B>(&mut self, task: T, when: Instant, tag: Tag<'static>)
     where
         T: Into<Box<B>>,
         B: Task + 'static,
     {
         self.scheduled_tasks
-            .push(ScheduledTask::new(sleep_until(when), tag, task.into()));
+            .push(ScheduledTask::new(when, tag, task.into()));
     }
 
-    fn schedule_delayed_with_tag<T, B>(&mut self, task: T, delay: Duration, tag: Tag<'static>)
+    pub fn schedule_delayed_with_tag<T, B>(&mut self, task: T, delay: Duration, tag: Tag<'static>)
     where
         T: Into<Box<B>>,
         B: Task + 'static,
@@ -105,15 +112,34 @@ impl Scheduler {
         self.schedule_with_tag(task, Instant::now() + delay, tag);
     }
 
-    fn cancel_tag(&mut self, tag: &Tag) {
-        // TODO: here
+    pub fn schedule_random_delay<T, B>(&mut self, task: T, min: f64, max: f64)
+    where
+        T: TaggedTask + Into<Box<B>>,
+        B: Task + 'static,
+    {
+        let dist = Uniform::new(min, max);
+        let delay: f64 = dist.sample(&mut rand::thread_rng());
+        self.schedule_delayed(task, Duration::from_secs_f64(delay));
     }
 
-    async fn next_task(&mut self) -> Option<(Tag<'static>, Box<dyn Task>)> {
+    pub fn cancel_tag(&mut self, tag: &Tag) {
+        self.scheduled_tasks = self
+            .scheduled_tasks
+            .drain()
+            .filter(|task| task.tag != *tag)
+            .collect();
+    }
+
+    fn pop(&mut self) -> Option<(Tag<'static>, Box<dyn Task>)> {
+        self.scheduled_tasks
+            .pop()
+            .map(|ScheduledTask { tag, task, .. }| (tag, task))
+    }
+
+    pub async fn next(&mut self) -> Option<(Tag<'static>, Box<dyn Task>)> {
         if let Some(task) = self.scheduled_tasks.peek() {
-            // task.sleep.await;
-            let ScheduledTask { tag, task, .. } = self.scheduled_tasks.pop().unwrap();
-            Some((tag, task))
+            sleep_until(task.time).await;
+            self.pop()
         } else {
             None
         }
@@ -121,18 +147,18 @@ impl Scheduler {
 }
 
 #[async_trait]
-trait Task: Send + Sync + Debug {
+pub trait Task: Send + Sync + Debug {
     async fn run(&self) -> Result<()>;
 }
 
-trait TaggedTask: Task {
+pub trait TaggedTask: Task {
     fn tag(&self) -> Tag<'static> {
         format!("{:?}", self).into()
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-struct SyncDotfiles;
+pub struct SyncDotfiles;
 
 #[async_trait]
 impl Task for SyncDotfiles {
@@ -145,7 +171,7 @@ impl Task for SyncDotfiles {
 impl TaggedTask for SyncDotfiles {}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-struct SyncPlugins;
+pub struct SyncPlugins;
 
 #[async_trait]
 impl Task for SyncPlugins {
@@ -156,6 +182,16 @@ impl Task for SyncPlugins {
 }
 
 impl TaggedTask for SyncPlugins {}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct NoopTask;
+
+#[async_trait]
+impl Task for NoopTask {
+    async fn run(&self) -> Result<()> {
+        Ok(())
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -198,11 +234,15 @@ mod tests {
 
         scheduler.schedule_with_tag(TestTask, Instant::now(), "test1".into());
         scheduler.schedule_with_tag(TestTask, Instant::now(), "test2".into());
+        scheduler.schedule_with_tag(TestTask, Instant::now(), "test2".into());
         scheduler.schedule_with_tag(TestTask, Instant::now(), "test3".into());
-
+        assert_eq!(scheduler.scheduled_tasks.len(), 4);
         scheduler.cancel_tag(&"test2".into());
-
-        assert_eq!(scheduler.scheduled_tasks.len(), 3);
+        assert_eq!(scheduler.scheduled_tasks.len(), 2);
+        scheduler.cancel_tag(&"test3".into());
+        assert_eq!(scheduler.scheduled_tasks.len(), 1);
+        scheduler.cancel_tag(&"test1".into());
+        assert_eq!(scheduler.scheduled_tasks.len(), 0);
     }
 
     #[tokio::test]
@@ -211,11 +251,14 @@ mod tests {
 
         scheduler.schedule_delayed_with_tag(TestTask, Duration::from_secs(0), "test1".into());
         scheduler.schedule_delayed_with_tag(TestTask, Duration::from_secs(1), "test2".into());
+        scheduler.schedule_delayed_with_tag(TestTask, Duration::from_secs(2), "test3".into());
 
+        assert_eq!(scheduler.scheduled_tasks.len(), 3);
+        assert_eq!(scheduler.pop().unwrap().0, "test1".into());
         assert_eq!(scheduler.scheduled_tasks.len(), 2);
-        assert_eq!(scheduler.next_task().await.unwrap().0, "test1".into());
+        assert_eq!(scheduler.pop().unwrap().0, "test2".into());
         assert_eq!(scheduler.scheduled_tasks.len(), 1);
-        assert_eq!(scheduler.next_task().await.unwrap().0, "test2".into());
+        assert_eq!(scheduler.pop().unwrap().0, "test3".into());
         assert_eq!(scheduler.scheduled_tasks.len(), 0);
     }
 }
