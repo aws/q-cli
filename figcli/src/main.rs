5bf36365b50d8aa423b098120e48b700f04f6642
grant0417 <grantgurvis@gmail.com>
2022-03-16 20:04:28 -0700

    figcli: add way to disable sentry

--- figcli/src/main.rs
+++ figcli/src/main.rs
@@ -9,20 +9,27 @@ use fig_auth::get_email;
 
 #[tokio::main]
 async fn main() {
-    let _guard = sentry::init((
-        "https://0631fceb9ae540bb874af81820507ebf@o436453.ingest.sentry.io/6187837",
-        sentry::ClientOptions {
-            release: sentry::release_name!(),
-            ..Default::default()
-        },
-    ));
+    let _guard = match std::env::var_os("FIG_DISABLE_SENTRY") {
+        Some(_) => None,
+        None => {
+            let guard = sentry::init((
+                "https://0631fceb9ae540bb874af81820507ebf@o436453.ingest.sentry.io/6187837",
+                sentry::ClientOptions {
+                    release: sentry::release_name!(),
+                    ..Default::default()
+                },
+            ));
 
-    sentry::configure_scope(|scope| {
-        scope.set_user(Some(sentry::User {
-            email: get_email(),
-            ..Default::default()
-        }));
-    });
+            sentry::configure_scope(|scope| {
+                scope.set_user(Some(sentry::User {
+                    email: get_email(),
+                    ..Default::default()
+                }));
+            });
+
+            Some(guard)
+        }
+    };
 
     cli::Cli::parse().execute().await;
 }
