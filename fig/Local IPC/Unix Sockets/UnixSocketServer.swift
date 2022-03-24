8ab5cc703d62e8423fd3aa291dcc562b3f3eca06
Matt Schrage <matthewschrage@gmail.com>
2022-03-23 18:16:51 -0700

    fix: add more permissive file permissions to unix socket dir

--- fig/Local IPC/Unix Sockets/UnixSocketServer.swift
+++ fig/Local IPC/Unix Sockets/UnixSocketServer.swift
@@ -43,7 +43,10 @@ class UnixSocketServer {
     let url = URL(fileURLWithPath: self.path)
     try? FileManager.default.createDirectory(at: url.deletingLastPathComponent(),
                                             withIntermediateDirectories: true,
-                                            attributes: nil)
+                                             // Create with rw-rw-rw- permissions so folder
+                                             // can be reused from separate accounts
+                                             // https://github.com/withfig/fig/issues/1140
+                                             attributes: [ .posixPermissions: 0o666 ])
     self.bidirectional = bidirectional
   }
 
