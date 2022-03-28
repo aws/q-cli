0c48a3c2b349116042f10335de48f8222e43faa1
grant0417 <grantgurvis@gmail.com>
2022-03-28 16:06:44 -0700

    figcli: add `plugin add`, `remove`, and `list`

--- figcli/src/cli/plugins.rs
+++ figcli/src/cli/plugins.rs
@@ -2,12 +2,13 @@ use std::time::Duration;
 
 use anyhow::{bail, Context, Result};
 use clap::Subcommand;
+use crossterm::style::Stylize;
 use fig_ipc::{connect_timeout, send_recv_message};
 use fig_proto::daemon::{
     daemon_response::Response, sync_command::SyncType, sync_response::SyncStatus, DaemonResponse,
 };
 use fig_settings::api_host;
-use reqwest::Url;
+use reqwest::{Client, Url};
 
 use super::OutputFormat;
 
@@ -40,8 +41,6 @@ impl PluginsSubcommands {
     pub async fn execute(&self) -> Result<()> {
         match self {
             PluginsSubcommands::Sync => {
-                println!();
-
                 let spinner =
                     spinners::Spinner::new(spinners::Spinners::Dots, "Syncing plugins".into());
 
@@ -74,17 +73,22 @@ impl PluginsSubcommands {
                         ..
                     }) => match sync_result.status() {
                         SyncStatus::Ok => {
-                            spinner.stop_with_message("✔️ Successfully synced plugins".into());
-                            println!();
-                            println!();
+                            spinner.stop_with_message(format!(
+                                "{} Successfully synced plugins\n",
+                                "✔️".green()
+                            ));
                         }
                         SyncStatus::Error => {
-                            spinner.stop_with_message("❌ Failed to sync plugins".into());
+                            spinner.stop_with_message(format!(
+                                "{} Failed to sync plugins\n",
+                                "✖️".red()
+                            ));
                             bail!(sync_result.error().to_string());
                         }
                     },
                     _ => {
-                        spinner.stop_with_message("❌ Failed to sync plugins".into());
+                        spinner
+                            .stop_with_message(format!("{} Failed to sync plugins\n", "✖️".red()));
                         bail!("Could not get diagnostics from daemon");
                     }
                 }
@@ -92,46 +96,155 @@ impl PluginsSubcommands {
                 Ok(())
             }
             PluginsSubcommands::Add { plugin } => {
-                println!("Installing plugin {}", plugin);
-                bail!("Not implemented");
+                let spinner = spinners::Spinner::new(
+                    spinners::Spinners::Arc,
+                    format!("Installing plugin {}", plugin),
+                );
+
+                let api_host = api_host();
+                let url = Url::parse(&format!("{api_host}/dotfiles/plugins/add/{plugin}"))?;
+
+                let token = fig_auth::get_token().await?;
+
+                let response = Client::new()
+                    .post(url)
+                    .bearer_auth(token)
+                    .header("Accept", "application/json")
+                    .send()
+                    .await?;
+
+                match handle_fig_response(response).await {
+                    Ok(_) => {
+                        spinner.stop_with_message(format!(
+                            "{} Successfully installed plugin\n",
+                            "✔️".green()
+                        ));
+                        println!(
+                            "Run {} to start useing the plugin in the current shell",
+                            "fig source".magenta()
+                        );
+                        Ok(())
+                    }
+                    Err(err) => {
+                        spinner
+                            .stop_with_message(
+                                format!("{} Failed to install plugin\n", "✘".red(),),
+                            );
+                        Err(err)
+                    }
+                }
             }
             PluginsSubcommands::Remove { plugin } => {
-                println!("Removing plugin {}", plugin);
-                bail!("Not implemented");
-            }
-            PluginsSubcommands::List { format, .. } => {
+                let spinner = spinners::Spinner::new(
+                    spinners::Spinners::Arc,
+                    format!("Removing plugin {}", plugin),
+                );
+
                 let api_host = api_host();
-                let url = Url::parse(&format!("{api_host}/plugins/all"))?;
+                let url = Url::parse(&format!("{api_host}/dotfiles/plugins/remove/{plugin}"))?;
 
-                let json: serde_json::Value =
-                    reqwest::get(url).await?.error_for_status()?.json().await?;
+                let token = fig_auth::get_token().await?;
 
-                if let Some(object) = json.as_object() {
-                    if let Some(plugins) = object.get("plugins") {
-                        if format == &OutputFormat::Plain {
-                            if let Some(plugins) = plugins.as_array() {
-                                for plugin in plugins {
-                                    if let Some(name) = plugin.get("name") {
-                                        if let Some(name) = name.as_str() {
-                                            println!("{}", name);
-                                        }
-                                    }
-                                }
-                            } else {
-                                bail!("Plugins in response is not an array");
-                            }
-                        } else {
-                            println!("{}", serde_json::to_string(&plugins)?);
-                        }
-                    } else {
-                        bail!("Could not find plugins in response");
+                let response = Client::new()
+                    .post(url)
+                    .bearer_auth(token)
+                    .header("Accept", "application/json")
+                    .send()
+                    .await?;
+
+                match handle_fig_response(response).await {
+                    Ok(_) => {
+                        spinner.stop_with_message(format!(
+                            "{} Successfully removed plugin\n",
+                            "✔️".green()
+                        ));
+                        println!(
+                            "Run {} to stop using the plugin in the current shell",
+                            "fig source".magenta()
+                        );
+                        Ok(())
                     }
-                } else {
-                    bail!("Response is not an object");
+                    Err(err) => {
+                        spinner
+                            .stop_with_message(format!("{} Failed to remove plugin\n", "✘".red(),));
+                        Err(err)
+                    }
+                }
+            }
+            PluginsSubcommands::List { format, installed } => {
+                let api_host = api_host();
+                let url = match installed {
+                    false => Url::parse(&format!("{api_host}/plugins/all"))?,
+                    true => Url::parse(&format!("{api_host}/dotfiles/plugins"))?,
+                };
+
+                let mut request = Client::new().get(url);
+
+                if *installed {
+                    let token = fig_auth::get_token().await?;
+                    request = request.bearer_auth(token)
                 }
 
-                Ok(())
+                let response = request.send().await?;
+
+                match handle_fig_response(response).await {
+                    Ok(response) => {
+                        let json: serde_json::Value = response.json().await?;
+
+                        if let Some(object) = json.as_object() {
+                            if let Some(plugins) = object.get("plugins") {
+                                if format == &OutputFormat::Plain {
+                                    if let Some(plugins) = plugins.as_array() {
+                                        for plugin in plugins {
+                                            if let Some(name) = plugin.get("name") {
+                                                if let Some(name) = name.as_str() {
+                                                    println!("{}", name);
+                                                }
+                                            }
+                                        }
+                                        Ok(())
+                                    } else {
+                                        bail!("Plugins in response is not an array");
+                                    }
+                                } else {
+                                    println!("{}", serde_json::to_string(&plugins)?);
+                                    Ok(())
+                                }
+                            } else {
+                                bail!("Could not find plugins in response");
+                            }
+                        } else {
+                            println!("{}", json);
+                            bail!("Response is not an object");
+                        }
+                    }
+                    Err(err) => Err(err),
+                }
             }
+        }
+    }
+}
+
+async fn handle_fig_response(resp: reqwest::Response) -> Result<reqwest::Response> {
+    if resp.status().is_success() {
+        Ok(resp)
+    } else {
+        let err = resp.error_for_status_ref().err();
+
+        match resp.json::<serde_json::Value>().await {
+            Ok(json) => {
+                let error = json
+                    .get("error")
+                    .and_then(|error| error.as_str())
+                    .unwrap_or("Unknown error")
+                    .to_string();
+
+                bail!(error)
+            }
+            Err(_) => match err {
+                Some(err) => bail!(err),
+                None => bail!("Unknown error"),
+            },
         }
     }
 }
