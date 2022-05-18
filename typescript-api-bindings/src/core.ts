820a24b19830a1ba61194e426931235a800d8141
grant0417 <grantgurvis@gmail.com>
2022-05-18 14:40:40 -0700

    Remove tauri from api bindings

--- typescript-api-bindings/src/core.ts
+++ typescript-api-bindings/src/core.ts
@@ -28,7 +28,7 @@ export function sendMessage(
 ) {
   const request: ClientOriginatedMessage = {
     id: (messageId += 1),
-    submessage: message,
+    submessage: message
   };
 
   if (handler && request.id) {
@@ -44,14 +44,6 @@ export function sendMessage(
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     window.ipc.postMessage(b64);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-  } else if (window?.__TAURI__?.invoke) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    window.__TAURI__.invoke("handle_api_request", {
-      clientOriginatedMessageB64: b64,
-    });
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
   } else if (window.webkit) {
@@ -70,7 +62,7 @@ export function sendMessage(
     console.warn(
       "Cannot send request. Fig.js is not supported in this browser."
     );
-    return;
+    
   }
 }
 
@@ -106,29 +98,6 @@ const setupEventListeners = (): void => {
   });
 };
 
-async function setupTauriEventListeners() {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  if (window?.__TAURI__?.event?.listen) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    await window.__TAURI__.event.listen(FigGlobalErrorOccurred, (event: any) => {
-      const response = { error: event.payload } as GlobalAPIError;
-      console.error(response);
-    });
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    await window.__TAURI__.event.listen(FigProtoMessageRecieved, (event: any) => {
-      const raw = event.payload as string;
-      const bytes = b64ToBytes(raw);
-      const message = ServerOriginatedMessage.decode(bytes);
-      recievedMessage(message);
-    });
-  }
-}
-
 // We want this to be run automatically
 console.log('[fig] setting up event listeners...');
 setupEventListeners();
-setupTauriEventListeners();
