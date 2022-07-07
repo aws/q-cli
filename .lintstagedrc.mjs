835630e2544723393bc6bd12e0e202f8719487e0
grant0417 <grantgurvis@gmail.com>
2022-07-06 19:43:19 -0700

    Ci related stuff

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -2,6 +2,6 @@ export default {
   "**/*.swift": ["swiftlint lint --fix --quiet 2>/dev/null", "swiftlint lint"],
   "**/*.rs": (files) => [
     `cargo +nightly fmt --check -- --color always ${files.join(" ")}`,
-    "cargo clippy --color always -- -D warnings",
+    "cargo +stable clippy --locked --color always -- -D warnings",
   ],
 };
