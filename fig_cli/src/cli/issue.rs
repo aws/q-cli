01b03f6d75e76b28110259366214ff8a9c42b18b
Robert <52195359+SeparateRecords@users.noreply.github.com>
2022-07-26 03:43:14 +1000

    refactor: `fig issue` uses issue template (#1097)
    * Change `fig issue` URL params
    
    * Remove html from details
    
    Co-authored-by: grant0417 <grantgurvis@gmail.com>

--- fig_cli/src/cli/issue.rs
+++ fig_cli/src/cli/issue.rs
@@ -38,45 +38,44 @@ impl IssueArgs {
             return Ok(());
         }
 
-        let text = self.description.join(" ");
+        let issue_title = self.description.join(" ");
         let mut assignees = vec!["mschrage"];
 
-        if Regex::new(r"(?i)cli").unwrap().is_match(&text) {
+        if Regex::new(r"(?i)cli").unwrap().is_match(&issue_title) {
             assignees.push("grant0417");
             assignees.push("sullivan-sean");
         }
 
-        if Regex::new(r"(?i)figterm").unwrap().is_match(&text) {
+        if Regex::new(r"(?i)figterm").unwrap().is_match(&issue_title) {
             assignees.push("grant0417");
             assignees.push("sullivan-sean");
         }
 
-        let mut body =
-            "### Description:\n> Please include a detailed description of the issue (and an image or screen \
-                    recording, if applicable)\n\n"
-                .to_owned();
-        if !text.is_empty() {
-            body.push_str(&text);
-        }
-        body.push_str("\n\n### Details:\n|OS|Fig|Shell|\n|-|-|-|\n");
+        let mut body = "### Details:\n|OS|Fig|Shell|\n|-|-|-|\n".to_owned();
 
         let os_version: String = OSVersion::new().map(|v| v.into()).unwrap_or_default();
         let fig_version = get_fig_version().map(|(version, _)| version).unwrap_or_default();
         let shell = get_parent_process_exe().unwrap_or_default();
         writeln!(body, "|{}|{}|{}|", &os_version, &fig_version, &shell.display()).ok();
-        body.push_str("<details><summary><code>fig diagnostic</code></summary>\n<p>\n\n");
+        body.push_str("fig diagnostic\n\n");
 
         let diagnostic = Diagnostics::new().await?.user_readable()?.join("\n");
         body.push_str(&diagnostic);
-        body.push_str("\n\n</p>\n</details>");
 
         println!("{}", &body);
 
         println!("\n→ Opening GitHub...\n");
 
         let url = url::Url::parse_with_params("https://github.com/withfig/fig/issues/new", &[
+            ("labels", "NEED_TO_LABEL"),
             ("assignees", &assignees.join(",")),
-            ("body", &body),
+            ("template", "1_main_issue_template.yml"),
+            (
+                "issue_details",
+                "<!-- Include a detailed description of the issue, and a screenshot/video if you can! -->\n\n",
+            ),
+            ("environment", &body),
+            ("title", &issue_title),
         ])?;
 
         if fig_util::open_url(url.as_str()).is_err() {
