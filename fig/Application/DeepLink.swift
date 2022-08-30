5f6b5eeabf60d31905c1814e21770b3056511c0c
Matt Schrage <matthewschrage@gmail.com>
2022-08-30 15:45:49 -0700

    feat: add generic deeplinking into Dashboard (#1354)

--- fig/Application/DeepLink.swift
+++ fig/Application/DeepLink.swift
@@ -17,8 +17,11 @@ class DeepLinkHandler {
   class func handle(appName: String?, path: String?, queries: [URLQueryItem]?) {
     if let name = appName {
       switch name {
+      // todo: fig://plugins is deprecated and should not be reimplemented. Instead use, fig://dashboard/plugins/...
       case "plugins":
         openPluginPage(path: path)
+      case "dashboard":
+        MissionControl.openUI(.home, additionalPathComponent: path)
       default:
         return
       }
