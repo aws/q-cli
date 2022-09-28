e071c0f4cfaba92cf8cc2103154c2c9709d442af
Grant G <grant0417@users.noreply.github.com>
2022-09-27 18:01:12 -0700

    feat: remove `protobuf-src` as dependency (#1548)
    * parse protoc version
    
    * remove proto_src dep
    
    * add protobuf install
    
    * ci: install protoc
    
    Co-authored-by: Mia <mia@fig.io>

--- rust-lib/fig_proto/build.rs
+++ rust-lib/fig_proto/build.rs
@@ -1,24 +1,43 @@
 use std::io::Result;
 
-const PROTO_FILES: &[&str] = &[
-    "../../proto/fig_common.proto",
-    "../../proto/local.proto",
-    "../../proto/figterm.proto",
-    "../../proto/daemon.proto",
-    "../../proto/fig.proto",
-    "../../proto/secure.proto",
-    "../../proto/stress.proto",
-];
+/// Try to find the version of protoc installed on the system.
+fn protoc_version() -> Option<[u32; 3]> {
+    let output = std::process::Command::new("protoc").arg("--version").output().ok()?;
+    let version = String::from_utf8(output.stdout).ok()?;
+    let version = version.trim();
+    let version = version.split(' ').last().expect("No version");
+    let version = version.split('.').collect::<Vec<_>>();
+    if version.len() != 3 {
+        return None;
+    }
+    let version = version
+        .iter()
+        .map(|s| s.parse::<u32>().ok())
+        .collect::<Option<Vec<_>>>()?;
+    Some([version[0], version[1], version[2]])
+}
 
 fn main() -> Result<()> {
-    for file in PROTO_FILES {
-        println!("cargo:rerun-if-changed={file}");
+    let proto_files = std::fs::read_dir("../../proto")?
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| entry.file_type().map(|t| t.is_file()).unwrap_or(false))
+        .filter(|entry| entry.path().extension().map(|ext| ext == "proto").unwrap_or(false))
+        .map(|entry| entry.path())
+        .collect::<Vec<_>>();
+
+    for file in &proto_files {
+        println!("cargo:rerun-if-changed={}", file.display());
     }
 
-    // TODO: remove this when protoc is newer in all repos
-    #[cfg(target_os = "linux")]
-    if std::env::var_os("LOCAL_PROTOC").is_none() {
-        std::env::set_var("PROTOC", protobuf_src::protoc());
+    // --experimental_allow_proto3_optional is supported only on version of protoc >= 3.12
+    // if the version of the system protoc is too old, we must panic
+    match protoc_version() {
+        Some(version @ ([0..=2, _, _] | [3, 0..=11, _])) => {
+            let version_str = version.iter().map(|v| v.to_string()).collect::<Vec<_>>().join(".");
+            panic!("protoc version {version_str} is too old, please install version 3.12 or newer",)
+        },
+        None => panic!("protoc not found"),
+        _ => (),
     }
 
     let mut config = prost_build::Config::new();
@@ -31,7 +50,7 @@ fn main() -> Result<()> {
         "#[cfg_attr(feature = \"arbitrary\", derive(arbitrary::Arbitrary))]",
     );
 
-    prost_reflect_build::Builder::new().compile_protos_with_config(config, PROTO_FILES, &["../../proto"])?;
+    prost_reflect_build::Builder::new().compile_protos_with_config(config, &proto_files, &["../../proto"])?;
 
     Ok(())
 }
