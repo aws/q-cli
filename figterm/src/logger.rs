c6729ecab7f865add9359fb531f92e0da21f86f2
grant0417 <grant@ggurvis.com>
2022-09-29 21:20:10 -0700

    fix: clippy lint

--- figterm/src/logger.rs
+++ figterm/src/logger.rs
@@ -4,7 +4,6 @@ use fig_log::get_max_fig_log_level;
 use tracing::Level;
 
 pub fn stdio_debug_log(s: impl Display) {
-    let max_level = get_max_fig_log_level();
     if get_max_fig_log_level() >= Level::DEBUG {
         println!("{s}");
     }
