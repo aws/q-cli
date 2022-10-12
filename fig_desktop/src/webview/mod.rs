f9a81add5392f0caf5006c03b2153aedc9b312d5
Chay Nabors <chaynabors@gmail.com>
2022-10-11 18:29:35 -0700

    fix: lint errors

--- fig_desktop/src/webview/mod.rs
+++ fig_desktop/src/webview/mod.rs
@@ -237,12 +237,12 @@ impl WebviewManager {
                             },
                             WryWindowEvent::Focused(true) => {
                                 if window_state.window_id == DASHBOARD_ID {
-                                    if let Some(autocomplete) = self.fig_id_map.get(&AUTOCOMPLETE_ID) {
-                                        proxy.send_event(Event::WindowEvent {
+                                    proxy
+                                        .send_event(Event::WindowEvent {
                                             window_id: AUTOCOMPLETE_ID,
                                             window_event: WindowEvent::Hide,
-                                        }).unwrap();
-                                    }
+                                        })
+                                        .unwrap();
                                 }
                             },
                             _ => (),
