f7cd52addb9890006628261eddd717d318018778
grant0417 <grantgurvis@gmail.com>
2022-10-19 18:35:14 -0700

    feat: improve updating screen a bit more

--- fig_desktop/src/install.rs
+++ fig_desktop/src/install.rs
@@ -138,6 +138,10 @@ pub async fn check_for_update(show_updating: bool) {
     use fig_install::UpdateStatus;
     use tokio::sync::mpsc::Receiver;
     use wry::application::dpi::LogicalSize;
+    use wry::application::menu::{
+        MenuBar,
+        MenuItem,
+    };
     use wry::application::platform::macos::WindowBuilderExtMacOS;
 
     let updating_cb: Option<Box<dyn FnOnce(Receiver<UpdateStatus>) + Send>> = if show_updating {
@@ -153,6 +157,11 @@ pub async fn check_for_update(show_updating: bool) {
             use wry::application::window::WindowBuilder;
             use wry::webview::WebViewBuilder;
 
+            let mut menu_bar = MenuBar::new();
+            let mut sub_menu_bar = MenuBar::new();
+            sub_menu_bar.add_native_item(MenuItem::Quit);
+            menu_bar.add_submenu("Fig", true, sub_menu_bar);
+
             let event_loop: EventLoop<UpdateStatus> = EventLoop::with_user_event();
             let window = WindowBuilder::new()
                 .with_title("Fig")
@@ -160,6 +169,7 @@ pub async fn check_for_update(show_updating: bool) {
                 .with_resizable(false)
                 .with_titlebar_hidden(true)
                 .with_movable_by_window_background(true)
+                .with_menu(menu_bar)
                 .build(&event_loop)
                 .unwrap();
 
@@ -167,12 +177,15 @@ pub async fn check_for_update(show_updating: bool) {
                 .unwrap()
                 .with_html(include_str!("../html/updating.html"))
                 .unwrap()
+                .with_devtools(true)
                 .build()
                 .unwrap();
 
+            // Forward recv to the webview
             let proxy = event_loop.create_proxy();
-
             std::thread::spawn(move || {
+                // Sleep for a little bit for the js to initialize (dont know why :()
+                std::thread::sleep(std::time::Duration::from_millis(500));
                 loop {
                     if let Some(event) = recv.blocking_recv() {
                         proxy.send_event(event).ok();
@@ -191,20 +204,19 @@ pub async fn check_for_update(show_updating: bool) {
                     Event::UserEvent(event) => match event {
                         UpdateStatus::Percent(p) => {
                             webview
-                                .evaluate_script(&format!("updateProgress({})", p as i32))
+                                .evaluate_script(&format!("updateProgress({});", p as i32))
                                 .unwrap();
                         },
                         UpdateStatus::Message(message) => {
-                            tracing::info!("{}", message);
                             webview
-                                .evaluate_script(&format!("updateMessage({})", serde_json::json!(message)))
+                                .evaluate_script(&format!("updateMessage({});", serde_json::json!(message)))
                                 .unwrap();
                         },
                         UpdateStatus::Exit => {
                             *control_flow = ControlFlow::Exit;
                         },
                     },
-                    _ => (),
+                    _ => {},
                 }
             });
         }))
