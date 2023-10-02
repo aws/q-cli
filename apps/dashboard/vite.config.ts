4eac164ed83e232863dcbc57ed3afb16f98eeb06
Grant Gurvis <grangurv@amazon.com>
2023-10-02 16:53:39 -0700

    Add csp to dashboard

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,10 +1,47 @@
 import path from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig, type HtmlTagDescriptor, type Plugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
+
+const csp: Record<string, string> = {
+  "default-src": "'self'",
+  "script-src": "'self'",
+  "img-src": "'self'",
+  "style-src": "'self'",
+  "connect-src": "'self'",
+  "object-src": "'none'",
+  "frame-src": "'none'",
+};
+
+const cspContent = Object.entries(csp)
+  .map(([k, v]) => `${k} ${v}`)
+  .join("; ");
+
+const htmlCspPlugin: Plugin = {
+  name: "html-csp",
+  transformIndexHtml: {
+    order: "post",
+    handler: (_html, ctx): HtmlTagDescriptor[] => {
+      if (ctx.server?.config?.mode === "development") {
+        return [];
+      }
+
+      return [
+        {
+          injectTo: "head",
+          tag: "meta",
+          attrs: {
+            "http-equiv": "Content-Security-Policy",
+            content: cspContent,
+          },
+        },
+      ];
+    },
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), htmlCspPlugin],
   server: {
     port: 3433,
   },
