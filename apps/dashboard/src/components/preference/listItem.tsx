2380cac1502aaa5f3145aca83a3e3e422c8c9a89
Bryn Jackson <jackbryn@amazon.com>
2023-10-02 16:35:01 -0700

    adds select type

--- apps/dashboard/src/components/preference/listItem.tsx
+++ apps/dashboard/src/components/preference/listItem.tsx
@@ -2,6 +2,14 @@ import { useEffect, useState } from "react";
 import { Switch } from "../ui/switch";
 import { State } from "@withfig/api-bindings";
 import { Pref, PrefDefault } from "@/types/preferences";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 export function Setting({ data }: { data: Pref }) {
   const [inputValue, setInputValue] = useState<PrefDefault>(data.default);
@@ -27,13 +35,13 @@ export function Setting({ data }: { data: Pref }) {
 
   return (
     <div className="flex p-4 pl-2 gap-4">
-      <div className="flex-none w-12 pt-1">
+      <div className="flex-none w-12">
         {data.type === "boolean" && (
           <Switch onClick={toggleSwitch} checked={localValue as boolean} />
         )}
       </div>
-      <div className="flex flex-col">
-        <h2 className="font-medium text-base">{data.title}</h2>
+      <div className="flex flex-col gap-1">
+        <h2 className="font-medium text-base leading-none">{data.title}</h2>
         {data.description && (
           <p className="font-light leading-tight">{data.description}</p>
         )}
@@ -41,13 +49,28 @@ export function Setting({ data }: { data: Pref }) {
           <p className="font-light leading-tight">{data.example}</p>
         )}
         {data.type !== "boolean" && (
-        <div>
-          {data.type === 'select' && <div />}
-          {data.type === 'multiselect' && <div />}
-          {data.type === 'number' && <div />}
-          {data.type === 'text' && <div />}
-          {data.type === 'keystrokes' && <div />}
-        </div>
+          <div className="pt-1">
+            {data.type === "select" && (
+              <Select>
+                <SelectTrigger className="w-60">
+                  <SelectValue placeholder={data.default} />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {data.options?.map((o, i) => (
+                      <SelectItem value={o} key={i}>
+                        {o}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            )}
+            {data.type === "multiselect" && <div />}
+            {data.type === "number" && <div />}
+            {data.type === "text" && <div />}
+            {data.type === "keystrokes" && <div />}
+          </div>
         )}
       </div>
     </div>
