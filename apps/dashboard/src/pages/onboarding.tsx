dd03a8110e75302fcf7ed144c1f27abf1b7ca21e
Bryn Jackson <jackbryn@amazon.com>
2023-10-25 10:30:31 -0700

    fixes getting started page for aws launch

--- apps/dashboard/src/pages/onboarding.tsx
+++ apps/dashboard/src/pages/onboarding.tsx
@@ -1,36 +1,28 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const tasks = [
   {
-    title: 'Open a new terminal emulator to get started'
+    title: "Configure your system",
+    description:
+      "Set up accessibility, input methods, and log in with your Builder ID.",
+    check: () => true,
   },
   {
-    title: "If something's not working, check out Help & Support"
+    title: "Choose a theme",
+    description: "Set the CodeWhisperer theme to fit your terminal",
+    check: () => false,
   },
   {
-    title: "Customize your settings to make"
-  }
-  // {
-  //   title: "Configure your system",
-  //   description:
-  //     "Set up accessibility, input methods, and log in with your Builder ID.",
-  //   check: () => true,
-  // },
-  // {
-  //   title: "Choose a theme",
-  //   description: "Set the CodeWhisperer theme to fit your terminal",
-  //   check: () => false,
-  // },
-  // {
-  //   title: "Customize tab behavior",
-  //   description: "",
-  //   check: () => true,
-  // },
-  // {
-  //   title: "Run `cw ai`",
-  //   description: "Convert natural language into Bash commands.",
-  //   check: () => false,
-  // },
+    title: "Customize tab behavior",
+    description: "",
+    check: () => true,
+  },
+  {
+    title: "Run `cw ai`",
+    description: "Convert natural language into Bash commands.",
+    check: () => false,
+  },
 ];
 
 type Task = {
@@ -39,16 +31,16 @@ type Task = {
   check?: () => boolean;
 };
 
-function Task({ task }: { task: Task, updateProgress: () => void}) {
+function Task({ task }: { task: Task; updateProgress: () => void }) {
   // const taskComplete = task.check() === true
 
   // useEffect(() => {
-    // updateProgress()
+  // updateProgress()
   // }, [taskComplete, updateProgress])
 
   return (
-    <div className="flex p-4">
-      <div className="w-12 flex-none flex flex-col"></div>
+    <div className="flex p-4 pl-0">
+      {/* <div className="w-12 flex-none flex flex-col"></div> */}
       <div className="flex flex-col">
         <h2 className="font-bold">{task.title}</h2>
         {task.description && <p>{task.description}</p>}
@@ -59,7 +51,7 @@ function Task({ task }: { task: Task, updateProgress: () => void}) {
 
 export default function Page() {
   // const { setModal } = useContext(ModalContext);
-  const [tasksCompleted, setTasksCompleted] = useState(1)
+  const [tasksCompleted, setTasksCompleted] = useState(1);
   // const [completionPercentage, setCompletionPercentage] = useState(tasksCompleted / tasks.length * 100);
 
   // useEffect(() => {
@@ -84,9 +76,27 @@ export default function Page() {
           {/* <span className="text-sm font-bold">{`${completionPercentage}% complete`}</span> */}
           {/* <Progress className="w-full" value={completionPercentage} /> */}
         </div>
-        {tasks.map((t, i) => (
+        <div className="flex p-4 pl-0">
+          {/* <div className="w-12 flex-none flex flex-col"></div> */}
+          <div className="flex flex-col">
+            <h2 className="font-bold">Open a new terminal to get started</h2>
+          </div>
+        </div>
+        <div className="flex p-4 pl-0">
+          {/* <div className="w-12 flex-none flex flex-col"></div> */}
+          <div className="flex flex-col">
+            <h2 className="font-bold">If it's not working, check out <Link to={'/help'} className="text-blue-500 underline decoration-1 underline-offset-1 hover:text-blue-800 hover:underline-offset-4 transition-all duration-100">Help & support</Link></h2>
+          </div>
+        </div>
+        <div className="flex p-4 pl-0">
+          {/* <div className="w-12 flex-none flex flex-col"></div> */}
+          <div className="flex flex-col">
+            <h2 className="font-bold">Customize your settings (like keybindings) in the relevant feature page</h2>
+          </div>
+        </div>
+        {/* {tasks.map((t, i) => (
           <Task task={t} key={i} updateProgress={() => setTasksCompleted(tasksCompleted + 1)}/>
-        ))}
+        ))} */}
       </div>
       {/* <Button
           onClick={startOnboarding}
