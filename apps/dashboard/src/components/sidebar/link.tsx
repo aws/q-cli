43e293cab699718edd5003c3fc823a08f14437f7
Bryn Jackson <jackbryn@amazon.com>
2023-10-27 13:31:26 -0700

    error state polish

--- apps/dashboard/src/components/sidebar/link.tsx
+++ apps/dashboard/src/components/sidebar/link.tsx
@@ -9,7 +9,11 @@ const activeStyles = cn(
 );
 const errorStyle = cn(
   defaultStyles,
-  "bg-red-300 hover:bg-red-300 text-black dark:text-black [&>span]:bg-black [&>span]:text-red-500 [&:hover>span]:text-red-500 [&:hover>span]:bg-white hover:text-white border-red-400"
+  "hover:bg-red-100 text-red-600 dark:text-white [&>span]:bg-red-500 [&>span]:text-white [&:hover>span]:text-white [&:hover>span]:bg-red-500 hover:text-red-600 border-red-400"
+);
+const activeErrorStyle = cn(
+  defaultStyles,
+  "bg-red-600 hover:bg-red-700 hover:text-white text-white dark:text-white [&>span]:bg-white [&>span]:text-red-500 [&:hover>span]:text-red-500 [&:hover>span]:bg-white hover:text-white border-red-600 hover:border-red-700"
 );
 
 export default function SidebarLink({
@@ -29,14 +33,19 @@ export default function SidebarLink({
     <NavLink
       to={path}
       className={({ isActive }) => 
-      error ? errorStyle :
+      error ? isActive ? activeErrorStyle : errorStyle :
       isActive ? activeStyles : defaultStyles}
     >
       <div className="flex flex-row items-center gap-2 fill-current whitespace-nowrap">
         {icon}
         <span className="text-base select-none">{name}</span>
       </div>
-      {count && (
+      {error && (
+        <span className="flex items-center justify-center leading-none p-1 px-2 rounded-full font-bold text-xs select-none">
+          !
+        </span>
+      )}
+      {count && !error && (
         <span className="flex items-center justify-center leading-none p-1 px-2 rounded-full font-medium text-sm select-none">
           {count > 9 ? "9+" : count}
         </span>
