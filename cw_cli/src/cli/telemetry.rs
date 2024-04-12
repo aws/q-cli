7ead79321c436a8704ed4935e721ddc256b5f0f6
Grant Gurvis <grangurv@amazon.com>
2024-04-12 16:01:42 -0700

    Fix telemetry status command
    cr: https://code.amazon.com/reviews/CR-121672523

--- cw_cli/src/cli/telemetry.rs
+++ cw_cli/src/cli/telemetry.rs
@@ -1,28 +1,49 @@
 use clap::Subcommand;
+use crossterm::style::Stylize;
 use eyre::Result;
 use serde_json::json;
+
+use super::OutputFormat;
+
+const TELEMETRY_ENABLED_KEY: &str = "telemetry.enabled";
 
 #[derive(Debug, PartialEq, Eq, Subcommand)]
 pub enum TelemetrySubcommand {
     Enable,
     Disable,
-    Status,
+    Status {
+        /// Format of the output
+        #[arg(long, short, value_enum, default_value_t)]
+        format: OutputFormat,
+    },
 }
 
 impl TelemetrySubcommand {
     pub async fn execute(&self) -> Result<()> {
         match self {
             TelemetrySubcommand::Enable => {
-                fig_settings::settings::set_value("telemetry.enabled", json!(true))?;
+                fig_settings::settings::set_value(TELEMETRY_ENABLED_KEY, true)?;
                 Ok(())
             },
             TelemetrySubcommand::Disable => {
-                fig_settings::settings::set_value("telemetry.enabled", json!(false))?;
+                fig_settings::settings::set_value(TELEMETRY_ENABLED_KEY, false)?;
                 Ok(())
             },
-            TelemetrySubcommand::Status => {
-                let status = fig_settings::settings::get_bool_or("telemetry.enabled", true);
-                println!("Telemetry is {}", if status { "disabled" } else { "enabled" });
+            TelemetrySubcommand::Status { format } => {
+                let status = fig_settings::settings::get_bool_or(TELEMETRY_ENABLED_KEY, true);
+                format.print(
+                    || {
+                        format!(
+                            "Telemetry status: {}",
+                            if status { "enabled" } else { "disabled" }.bold()
+                        )
+                    },
+                    || {
+                        json!({
+                            TELEMETRY_ENABLED_KEY: status,
+                        })
+                    },
+                );
                 Ok(())
             },
         }
