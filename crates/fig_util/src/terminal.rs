24bf1b52c5a3df81fc617eb50d87e55e2136d6d4
codeime <bingguo93@outlook.com>
2024-11-27 17:28:52 +0800

    Revert "feat: VSCodeVariant add windsurf (#125)"
    This reverts commit 7305bcf9718be6c093feb2cfc3f9018ef4bc742d.

--- crates/fig_util/src/terminal.rs
+++ crates/fig_util/src/terminal.rs
@@ -23,7 +23,6 @@ pub const MACOS_TERMINALS: &[Terminal] = &[
     Terminal::Cursor,
     Terminal::CursorNightly,
     Terminal::Rio,
-    Terminal::Windsurf,
 ];
 
 /// Terminals that Linux supports
@@ -133,8 +132,6 @@ pub enum Terminal {
     Nvim,
     /// Zellij
     Zellij,
-    /// Windsurf
-    Windsurf,
 }
 
 impl fmt::Display for Terminal {
@@ -169,7 +166,6 @@ impl fmt::Display for Terminal {
             Terminal::Cursor => write!(f, "Cursor"),
             Terminal::CursorNightly => write!(f, "Cursor Nightly"),
             Terminal::Rio => write!(f, "Rio"),
-            Terminal::Windsurf => write!(f, "Windsurf"),
         }
     }
 }
@@ -321,7 +317,6 @@ impl Terminal {
             Terminal::Cursor => "cursor".into(),
             Terminal::CursorNightly => "cursor-nightly".into(),
             Terminal::Rio => "rio".into(),
-            Terminal::Windsurf => "windsurf".into(),
         }
     }
 
@@ -346,7 +341,6 @@ impl Terminal {
             Terminal::Cursor => Some("com.todesktop.230313mzl4w4u92".into()),
             Terminal::CursorNightly => Some("com.todesktop.23052492jqa5xjo".into()),
             Terminal::Rio => Some("com.raphaelamorim.rio".into()),
-            Terminal::Windsurf => Some("com.exafunction.windsurf".into()),
             _ => None,
         }
     }
@@ -369,7 +363,6 @@ impl Terminal {
             "com.todesktop.230313mzl4w4u92" => Terminal::Cursor,
             "com.todesktop.23052492jqa5xjo" => Terminal::CursorNightly,
             "com.raphaelamorim.rio" => Terminal::Rio,
-            "com.exafunction.windsurf" => Terminal::Windsurf,
             // todo(mschrage): the following line does not account for Android Studio
             _ if bundle.starts_with("com.jetbrains.") | bundle.starts_with("com.google.") => {
                 Terminal::IntelliJ(IntelliJVariant::from_bundle_id(bundle))
@@ -416,7 +409,6 @@ impl Terminal {
                 | Terminal::Tabby
                 | Terminal::Cursor
                 | Terminal::CursorNightly
-                | Terminal::Windsurf
         )
     }
 
@@ -440,7 +432,6 @@ impl Terminal {
             Terminal::Cursor => &["Cursor", "cursor"],
             Terminal::CursorNightly => &["Cursor Nightly", "cursor-nightly"],
             Terminal::Rio => &["rio"],
-            Terminal::Windsurf => &["windsurf"],
 
             Terminal::Ssh => &["sshd"],
             Terminal::Tmux => &["tmux", "tmux: server"],
@@ -505,7 +496,6 @@ impl Terminal {
                 | Terminal::VSCodium
                 | Terminal::Cursor
                 | Terminal::CursorNightly
-                | Terminal::Windsurf
         )
     }
 
